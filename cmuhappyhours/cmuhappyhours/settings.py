a49a1f690429a4a985d6c5123155650d57d97c7c
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-11-27 00:15:59 -0500

    Added dotcloud.yml file

--- cmuhappyhours/cmuhappyhours/settings.py
+++ cmuhappyhours/cmuhappyhours/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Mihir Pandya', 'mpandya@cmu.edu'),
 )
 
 MANAGERS = ADMINS
--- cmuhappyhours/happyhours/models.py
+++ cmuhappyhours/happyhours/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Courses(models.Model):
+	course_id = models.IntegerField(max_length=10, primary_key=True)
+	course_name = models.CharField(max_length=150)
+
+class TA(models.Model):
+	u_id = models.AutoField(primary_key=True)
+	fname = models.CharField(max_length=30)
+	lname = models.CharField(max_length=30)
+	courseNum = models.ForeignKey(Courses)
+
+class OfficeHours(models.Model):
+	o_id = models.AutoField(primary_key=True)
+	ta_id = models.ForeignKey(TA)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	venue = models.CharField(max_length=200)
