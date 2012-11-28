ba26025c72a81fad9ed06aa0ea02cc50762786fc
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-11-28 00:49:03 -0500

    Added search bar. Doesn't work on dotcloud yet.

--- cmuhappyhours/cmuhappyhours/settings.py
+++ cmuhappyhours/cmuhappyhours/settings.py
@@ -9,7 +9,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))
 
 if pwd.getpwuid(os.getuid())[0] == 'dotcloud':
     envfile = '/home/dotcloud/environment.json'
-    STATIC_ROOT = PROJECT_ROOT
+    STATIC_ROOT = '/home/dotcloud/volatile/static/'
     STATIC_URL = 'static/'
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static/'),
@@ -82,40 +82,6 @@ USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'j!y9z7v#otqjhk6psj0=#t@jyb%62(b0a=+%v_5f@$m58i(k%2'
